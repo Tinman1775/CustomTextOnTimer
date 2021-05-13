57ab91b48a5b92bc123f2bd49971d382cfb7bb39
Director Krennic <tinman44281@gmail.com>
2021-05-12 22:56:07 -0400

    CustomTextOnTimer
    Allows for a configurable text chat message that sends at a configurable interval

---
+++ RespawnReminderV2/CustomTextPatch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VirtualBrightPlayz.SCP_ET;
+using UnityEngine;
+using PluginFramework;
+using HarmonyLib;
+using RespawnReminderV2;
+using System.Threading.Tasks;
+using VirtualBrightPlayz.SCP_ET.ServerConsole;
+using UnityEngine;
+using System.Collections;
+using Mirage;
+using VirtualBrightPlayz.SCP_ET.Player;
+using VirtualBrightPlayz.SCP_ET.World.Events;
+
+namespace CustomTextTimer
+{
+    [HarmonyPatch(typeof(IntroTeleport), nameof(IntroTeleport.StartRound))]
+    public static class CustomTextPatch
+    {
+        private const int MonkeDelay = 300000;
+        public static void Postfix(IntroTeleport __instance)
+        {
+           // Loop(__instance);
+
+        }
+    }
+    [HarmonyPatch(typeof(RoundEnd), nameof(RoundEnd.Update))]
+    public static class UpdatePatch
+    {
+        public static float timer = 0;
+        public static void Postfix(RoundEnd __instance)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                timer = MainClass.config.TimerMessageInterval;
+
+                GameObject.FindObjectOfType<PlayerMain>().textChat.RpcAddMessage(MainClass.config.MessageFormat, MainClass.config.MessageColor);
+            }
+        }
+    }
+}
---
+++ RespawnReminderV2/MonkeFolder/MainClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using PluginFramework;
+using PluginFramework.Logging;
+using UnityEngine;
+using HarmonyLib;
+using Newtonsoft.Json;
+namespace RespawnReminderV2
+{
+    public class MainClass : Plugin
+    {
+        public static Harmony inst;
+
+        public static MainClass.ReminderConfig config;
+
+        public override void OnDisable()
+        {
+            MainClass.inst.UnpatchAll(null);
+            MainClass.inst = null;
+        }
+
+        public override void OnEnable()
+        {
+            PluginSystem.Manager.Logger.Info("customTextTimer", "INIT");
+            MainClass.inst = new Harmony("customTextTimer");
+            MainClass.inst.PatchAll();
+            string str = Path.Combine(Application.dataPath, "../settings/customTextTimer.json");
+            if (!File.Exists(str))
+            {
+                File.WriteAllText(str, JsonConvert.SerializeObject(new MainClass.ReminderConfig()));
+            }
+
+            MainClass.config = JsonConvert.DeserializeObject<MainClass.ReminderConfig>(File.ReadAllText(str));
+        }
+
+        public class ReminderConfig
+        {
+            public string MessageFormat { get; set; } = "This is default, please add your message";
+
+            public float TimerMessageInterval { get; set; } = 60;
+
+            public string MessageColor { get; set; } = "#FFFF00";
+        }
+    }
+}
